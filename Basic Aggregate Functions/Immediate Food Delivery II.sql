1decc2471e4f18c150d250f3b8af042c77b8429e
Tanushree Pal <tanushreepal2005@gmail.com>
2026-09-26 09:06:08 +0530

    Solved Immediate Food Delivery II.

---
+++ Basic Aggregate Functions/Immediate Food Delivery II.sql
@@ -0,0 +1,10 @@
+WITH first_order AS(
+    SELECT 
+        customer_id,
+        MIN(order_date) AS orderD,
+        MIN(customer_pref_delivery_date) AS deliD
+        FROM Delivery
+        GROUP BY customer_id
+)
+SELECT ROUND(AVG(CASE WHEN f.orderD = f.deliD THEN 1 ELSE 0 END )*100,2) as immediate_percentage
+FROM first_order f;
